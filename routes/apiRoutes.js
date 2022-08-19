614e1f7870eca063a4e23c249568b6b8105ea25d
Jarmaris Chaney <jarmaris.chaney@yahoo.com>
2022-08-19 18:17:51 -0500

    updated apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,3 +10,19 @@ router.get('/notes', (req, res) => {
         })
 })
 
+router.post('/notes',(req, res)=> {
+    const newEntry = {
+        ...req.body,
+        id:uuid()
+    }
+    fs.readFile(path.join(__dirname, "../db/db.json"))//Reading in existing database from file
+        .then((fileContent) => {
+            const dbData = JSON.parse(fileContent)//turn text from the file into data
+            dbData.push(newEntry) //push the body of the request as a new item in the database array
+            return fs.writeFile(path.join(__dirname, "../db/db.json"),JSON.stringify(dbData))//writing changes back out to the file(db.json)
+        })
+        .then(()=>{
+            res.json(newEntry)
+        })
+})
+module.exports = router;
